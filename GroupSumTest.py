02c535d54b2ef0f84c5e0b23978f79060524560a
vector.tu <vector.tu@vivotek.com>
2018-01-31 23:05:44 +0800

    remove ItemGroup class in testing code.

--- GroupSumTest.py
+++ GroupSumTest.py
@@ -2,18 +2,6 @@ import unittest
 from GroupSum import ItemGroup
 from GroupSum import Item
 
-class ItemGroup:
-
-    def __init__(self, items):
-        self._items = items
-
-    def get_group_sum_set(self, tag, num_of_a_group):
-        ret_set = [0] * (len(self._items)/num_of_a_group + 1
-                            if len(self._items)%num_of_a_group else 0)
-        for idx, val in enumerate(self._items):
-            ret_set[idx/num_of_a_group] += getattr(val, tag)
-        return ret_set
-
 class ItemGroupTest(unittest.TestCase):
     items = [Item(Id=1,  Cost=1,  Revenue=11, SellPrice=21),
              Item(Id=2,  Cost=2,  Revenue=12, SellPrice=22),
